943c6858a1bf9804da6ee012062ff2320f364bf7
sczzlh <sczzlh@sina.com>
2018-03-01 23:38:36 +0800

    进度条

--- assets/Script/load.js
+++ assets/Script/load.js
@@ -2,9 +2,14 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        speed: 0.01,
         label: {
             default: null,
             type: cc.Label
+        },
+        loadBar: {
+            default: null,
+            type: cc.ProgressBar
         },
         // defaults, set visually when attaching this script to the Canvas
         text: '欢迎来到小偷世界!'
@@ -13,10 +18,15 @@ cc.Class({
     // use this for initialization
     onLoad: function () {
         this.label.string = this.text;
+        this.loadBar.progress = 0;
     },
 
     // called every frame
     update: function (dt) {
-
+        var progress = this.loadBar.progress;
+        if (progress < 1.0) {
+            progress += this.speed * dt;
+        }
+        this.loadBar.progress += progress;
     },
 });
