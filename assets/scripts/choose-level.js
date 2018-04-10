4ac709e6c9e165a0c356cbea581586a089f367ac
sczzlh <sczzlh@sina.com>
2018-04-11 00:06:55 +0800

    关卡

---
+++ assets/scripts/choose-level.js
@@ -0,0 +1,34 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // PageViewLabel: {
+        //     default: null,
+        //     type: cc.Label
+        // },
+    },
+
+    onLoad: function () {
+        this.pageIndex = 0;
+        // this.PageViewLabel.string = PageNames[this.pageIndex];
+
+    },
+    pageViewClick: function (event, coustomData) {
+        let node = event.node;
+        this.pageIndex = node.getComponent(cc.PageView).getCurrentPageIndex();
+        // this.PageViewLabel.string = PageNames[this.pageIndex];
+        cc.log('page index ' + this.pageIndex);
+        // let content = node.getComponent(cc.PageView).content;
+        // for (let i = 0; i < content.children.length; i++) {
+        //     content.children[i].setScale(0.8);
+        //     if (i === this.pageIndex) {
+        //         content.children[i].setScale(1);
+        //     }
+        // }
+
+    },
+    startGameClick: function (event, coustomData) {
+        cc.log('page index' + this.pageIndex);
+        cc.director.loadScene("game"/*'Game_' + (this.pageIndex + 1)*/);
+    }
+});
---
+++ assets/scripts/menu-button.js
@@ -0,0 +1,17 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    onLoad () {
+
+    },
+
+    clickAdventureButton () {
+        cc.director.loadScene("stage.fire");
+    }
+
+    // update (dt) {},
+});
