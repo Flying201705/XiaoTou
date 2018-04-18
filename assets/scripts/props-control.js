3dbf069a6a739856b4ccff5ec8505e6d86e21df3
sczzlh <sczzlh@sina.com>
2018-04-18 22:19:49 +0800

    界面道具添加

---
+++ assets/scripts/props-control.js
@@ -0,0 +1,19 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+    
+    onPressProp1Btn: function () {
+        cc.log("施放道具1");
+    },
+
+    onPressProp2Btn: function () {
+        cc.log("施放道具2");
+    },
+
+    onPressProp3Btn: function () {
+        cc.log("施放道具3");
+    },
+});
