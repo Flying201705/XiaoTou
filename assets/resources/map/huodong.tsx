b7f98d052e7aebc6a07c97fd525c87872b33bd2c
novaxx <zhang.1220@163.com>
2018-05-22 22:40:51 +0800

    提交代码：上传36-41关卡地图

---
+++ assets/resources/map/huodong.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="huodong" tilewidth="80" tileheight="80" tilecount="40" columns="5">
+ <image source="huodong.png" width="400" height="640"/>
+</tileset>
