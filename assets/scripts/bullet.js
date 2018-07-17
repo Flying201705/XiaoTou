3ce6dab637086eb7db8618e5f1db24efa4904108
sczzlh <sczzlh@sina.com>
2018-07-17 16:53:17 +0800

    子弹对象池

--- assets/scripts/bullet.js
+++ assets/scripts/bullet.js
@@ -1,3 +1,5 @@
+import global from "./global";
+
 cc.Class({
     extends: cc.Component,
 
@@ -5,6 +7,7 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
+        this.type = -1;
         this.direction = cc.p(0, 0);
         this.speed = 600;
     },
@@ -15,19 +18,25 @@ cc.Class({
         let angle = cc.pAngleSigned(this.direction, cc.p(0, 1));
         this.node.rotation = (180 / Math.PI) * angle;
         this.enemyNodeList = enemyNodeList;
-        this.damage = tower.getComponent("tower").getDamage();
-        //抢钱比例
-        this.gainRate = tower.getComponent("tower").getGainRate();
-        //眩晕率
-        this.stunRate = tower.getComponent("tower").getStunRate();
-        //暴击率
-        this.critRate = tower.getComponent("tower").getCritRate();
-        //减速率
-        this.slowRate = tower.getComponent("tower").getSlowRate();
-        //范围攻击
-        this.areaAtt = tower.getComponent("tower").isAreaAttack();
-        //攻击列表
-        this.attList = tower.getComponent("tower").getAreaEnemyList();
+
+        let towerScript = tower.getComponent("tower");
+        if (towerScript) {
+            this.type = towerScript.bulletType;
+            //伤害
+            this.damage = towerScript.getDamage();
+            //抢钱比例
+            this.gainRate = towerScript.getGainRate();
+            //眩晕率
+            this.stunRate = towerScript.getStunRate();
+            //暴击率
+            this.critRate = towerScript.getCritRate();
+            //减速率
+            this.slowRate = towerScript.getSlowRate();
+            //范围攻击
+            this.areaAtt = towerScript.isAreaAttack();
+            //攻击列表
+            this.attList = towerScript.getAreaEnemyList();
+        }
     },
 
     update: function (dt) {
@@ -44,21 +53,18 @@ cc.Class({
                     } else {
                         enemy.getComponent("enemy").beAttacked(this);
                     }
-                    this.node.destroy();
-                    // cc.log("")
-                    break;
+                    global.event.fire("destroy_bullet", this.type, this.node);
                 }
             }
         }
 
         if (this.node.position.x < -960 * 0.5 || this.node.position.x > 960 * 0.5
             || this.node.position.y > 640 * 0.5 || this.node.position.y < -640 * 0.5) {
-            this.node.destroy();
-            //cc.log("删掉子弹");
+            global.event.fire("destroy_bullet", this.type, this.node);
         }
 
     },
-    handleAreaAttack: function() {
+    handleAreaAttack: function () {
         for (let i = 0; i < this.attList.length; i++) {
             let enemy = this.attList[i];
             enemy.getComponent("enemy").beAttacked(this);
