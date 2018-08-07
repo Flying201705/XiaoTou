42479b7df29c72404f5b4ac32550198d5e7acba5
bunny <supercarabbit@qq.com>
2018-08-07 20:31:57 +0800

    英雄操作面板显示道具图标。

--- assets/scripts/actors/hero_panel.js
+++ assets/scripts/actors/hero_panel.js
@@ -8,18 +8,30 @@
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
 
+import {InfoData} from "../InfoData";
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
         title: cc.Label,
-        level: cc.Label
+        level: cc.Label,
+        props: [cc.Sprite],
+        propIcons: [cc.SpriteFrame]
     },
 
     onLoad() {
         this.title.string = '小兵之神';
     },
+    start() {
+        this._showProp();
+    },
     setLevel(level) {
-        this.level.string =`Lv${level}`;
+        this.level.string = `Lv${level}`;
+    },
+    _showProp() {
+        if (InfoData.user.prop_boot > 0) {
+            this.props[0].spriteFrame = this.propIcons[0];
+        }
     }
 });
