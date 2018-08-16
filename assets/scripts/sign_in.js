66a3a680cffb0a5af294b9f634ac1593dd46aadb
sczzlh <sczzlh@sina.com>
2018-08-16 16:25:11 +0800

    签到分享双倍

--- assets/scripts/sign_in.js
+++ assets/scripts/sign_in.js
@@ -14,11 +14,19 @@ cc.Class({
     properties: {
         gotFlag: [cc.Node],
         moreDayNode: cc.Node,
-        moreDayLabel: cc.Label
+        moreDayLabel: cc.Label,
+        dailyButton: cc.Button,
+        controlNode: cc.Node
     },
 
-    start() {
-
+    onLoad() {
+        if (config.aliveFunEnable === true) {
+            this.dailyButton.node.active = false;
+            this.controlNode.active = true;
+        } else {
+            this.dailyButton.node.active = true;
+            this.controlNode.active = false;
+        }
     },
     /**
      * 初始化签到天数显示UI
@@ -41,7 +49,29 @@ cc.Class({
      * 签到
      */
     signIn() {
-        this.node.parent.removeChild(this.node);
+        this.hide();
+        remoteHelper.signIn(InfoData.user.id, data => {
+            //签到成功领取水晶界面提示
+            global.event.fire("add_reward_hint", data);
+            new InfoHandle().updateLocalCrystal(data);
+            // if (config.aliveFunEnable === true) {
+            //     this._showWxShare();
+            // }
+        });
+    },
+    _showWxShare() {
+        // if (this.signDays > 1) {
+            WxHelper.share('normal', 'server');
+        // }
+    },
+
+    hide() {
+        this.node.removeFromParent();
+        this.node.destroy();
+    },
+
+    onClickDoubleBtn() {
+        this.hide();
         remoteHelper.signIn(InfoData.user.id, data => {
             //签到成功领取水晶界面提示
             global.event.fire("add_reward_hint", data);
@@ -50,10 +80,5 @@ cc.Class({
                 this._showWxShare();
             }
         });
-    },
-    _showWxShare() {
-        if (this.signDays > 1) {
-            WxHelper.share('normal', 'server');
-        }
     }
 });
