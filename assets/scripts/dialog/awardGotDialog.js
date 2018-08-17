21444b948f8b6d96b1a723529179965bbb65491b
bunny <supercarabbit@qq.com>
2018-08-17 11:42:49 +0800

    新手礼包显示奖励物品弹窗

--- assets/scripts/dialog/awardGotDialog.js
+++ assets/scripts/dialog/awardGotDialog.js
@@ -1,41 +1,61 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        itemContainer: cc.Node,
+        itemPrefab: cc.Prefab
     },
-
-    // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
-    start () {
+    start() {
 
     },
+    config(configs) {
+        if (configs.length == 1) {
+            let itemNode = this._getItem(configs[0]);
+            this.itemContainer.addChild(itemNode);
+        } else if (configs.length > 1) {
+            let itemNodeLeft = this._getItem(configs[0], 'left');
+            let itemNodeRight = this._getItem(configs[1], 'right');
+            this.itemContainer.addChild(itemNodeLeft);
+            this.itemContainer.addChild(itemNodeRight);
+        }
+    },
+    onClickOk() {
+        this.node.removeFromParent();
+        this.node.destroy();
+    },
+    _getItem(conf, position) {
+        let item = cc.instantiate(this.itemPrefab).getComponent('awardGotItem');
+        item.set(this._getSprite(conf.type), conf.count);
+        switch (position) {
+            case 'left':
+                item.node.setPositionX(-this.itemContainer.width / 2);
+                break;
+            case 'right':
+                item.node.setPositionX(this.itemContainer.width / 2);
+                break;
+        }
+        return item.node;
+    },
+    _getSprite(type) {
+        let sf = null;
+        switch (type) {
+            case 0:
+                sf = new cc.SpriteFrame(cc.url.raw('resources/image/crystal.png'));
+                break;
+            case 1:
+                sf = new cc.SpriteFrame(cc.url.raw('resources/image/deceleration.png'));
+                break;
+            case 2:
+                sf = new cc.SpriteFrame(cc.url.raw('resources/image/dizziness.png'));
+                break;
+            case 3:
+                sf = new cc.SpriteFrame(cc.url.raw('resources/image/bomb.png'));
+                break;
+        }
 
-    // update (dt) {},
+        return sf;
+    }
 });
